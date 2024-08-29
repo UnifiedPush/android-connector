0ac234acd5ea5052f8d14a62e9c60605887aa0a3
S1m <git@sgougeon.fr>
2024-08-29 16:58:47 +0200

    Fix publish script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -e
 
 NORMAL=$'\e[0m'
 GREEN=$(tput setaf 2)
@@ -45,8 +45,8 @@ rm -rf ~/.m2/repository/${DIR:?}
 cd ~/.m2/repository/
 for file in ${DIR:?}/*; do
     echo "[+] Signing $file"
-    sha1sum $file > $file.sha1
-    md5sum $file > $file.md5
+    sha1sum $file | sed 's/ .*//' > $file.sha1
+    md5sum $file | sed 's/ .*//' > $file.md5
     gpg -ab $file
 done
 
