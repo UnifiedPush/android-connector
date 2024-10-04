3ce82f1a8c4a3d7ad76f39baebc6d72e8fe1418a
sim <git@sgougeon.fr>
2024-10-04 09:29:08 +0200

    Add RaiseToForegroundService's documentation

--- connector/src/main/java/org/unifiedpush/android/connector/RaiseToForegroundService.kt
+++ connector/src/main/java/org/unifiedpush/android/connector/RaiseToForegroundService.kt
@@ -5,6 +5,15 @@ import android.content.Intent
 import android.os.Binder
 import android.os.IBinder
 
+/**
+ * @hide
+ *
+ * Service to raise the application to foreground as defined in the specifications
+ *
+ * The distributor can bind with foreground importance to this service for 5 seconds
+ * to raise the application to the foreground, allowing it to start a foreground
+ * service from the background during this 5 seconds
+ */
 class RaiseToForegroundService: Service() {
     override fun onBind(intent: Intent?): IBinder {
         return binder
